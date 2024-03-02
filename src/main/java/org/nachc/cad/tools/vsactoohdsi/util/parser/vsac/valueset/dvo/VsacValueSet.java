80e13a2922986dfb7ad24b24882bf8442f5774f6
NACHC-CAD <greshje.nachc@gmail.com>
2024-03-02 18:51:24 -0500

    adding value set parser and test

---
+++ src/main/java/org/nachc/cad/tools/vsactoohdsi/util/parser/vsac/valueset/dvo/VsacValueSet.java
@@ -0,0 +1,14 @@
+package org.nachc.cad.tools.vsactoohdsi.util.parser.vsac.valueset.dvo;
+
+import java.util.ArrayList;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class VsacValueSet {
+
+	private ArrayList<VsacValueSetRow> rows = new ArrayList<VsacValueSetRow>();
+	
+}
---
+++ src/main/java/org/nachc/cad/tools/vsactoohdsi/util/parser/vsac/valueset/dvo/VsacValueSetRow.java
@@ -0,0 +1,28 @@
+package org.nachc.cad.tools.vsactoohdsi.util.parser.vsac.valueset.dvo;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class VsacValueSetRow {
+
+	private String OID = null;
+	
+	private String ValueSetName = null;
+	
+	private String ExpansionVersion = null;
+	
+	private String Code = null;
+	
+	private String Descriptor = null;
+	
+	private String CodeSystemName = null;
+	
+	private String CodeSystemVersion = null;
+	
+	private String CodeSystemOID = null;
+	
+	private String Purpose = null;
+
+}
